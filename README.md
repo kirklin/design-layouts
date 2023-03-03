d7e7d165aa647e377545d5395362bed238476ead
Kirk Lin <linkirk@163.com>
2023-03-03 15:57:46 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Design Layouts
+Design Layouts is a website that showcases various layouts built using the Tailwind CSS framework. It's a collection of handcrafted layouts that you can use in your next project.
+
+## Setup
+
+Install dependencies:
+
+```bash
+pnpm install
+```
+
+## Development
+
+```bash
+pnpm dev
+```
+
---
+++ app.config.ts
@@ -0,0 +1,25 @@
+export default defineAppConfig({
+  docus: {
+    title: "Design Layouts",
+    description: "website that showcases various layouts.",
+    socials: {
+      github: "kirklin/design-layouts",
+    },
+    aside: {
+      level: 0,
+      exclude: [],
+    },
+    header: {
+      logo: false,
+    },
+    footer: {
+      credits: {
+        icon: '',
+        text: '',
+        href: '',
+      },
+      iconLinks: [
+      ],
+    },
+  },
+});
